aefc6d5ca547d78e08da34e85f3f7e9d339f021b
Dell <fafo2005hassanbh@gmail.com>
2026-09-11 17:37:29 +0300

    a

---
+++ level5/saveandmiss.go
@@ -0,0 +1,26 @@
+package piscine
+
+func SaveAndMiss(arg string, num int) string {
+	if num <= 0 || len(arg) == 0 {
+		return arg
+	}
+
+	runes := []rune(arg)
+	var res []rune
+	save := true
+
+	for i := 0; i < len(runes); i += num {
+		end := i + num
+		if end > len(runes) {
+			end = len(runes)
+		}
+
+		if save {
+			res = append(res, runes[i:end]...)
+		}
+
+		save = !save
+	}
+
+	return string(res)
+}
